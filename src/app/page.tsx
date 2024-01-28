c0375496a486ca71c48c84bae0a16b449bed93c6
pankaj suthar <pankajss0070@gmail.com>
2024-01-28 16:56:46 +0530

    done with login and logout with github in oauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { Button } from "@nextui-org/react";
-
-export default function Home() {
+import { auth } from "@/auth";
+import * as actions from "@/actions";
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Sign out</div>
+      )}
     </div>
   );
 }
