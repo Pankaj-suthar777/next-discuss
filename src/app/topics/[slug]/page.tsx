7d9b5ef2db8a7876d5304d9ebb928a81bbfba1fe
pankaj suthar <pankajss0070@gmail.com>
2024-01-30 21:18:41 +0530

    fronted fo component post

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/post/post-create-form";
+import PostList from "@/components/posts/post-list";
+import { fetchPostByTopicSlug } from "@/db/queries/post";
 
 interface TopicShowProps {
   params: {
@@ -12,6 +14,7 @@ const topicShowPage = ({ params }: TopicShowProps) => {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostByTopicSlug(slug)}></PostList>
       </div>
       <div>
         <PostCreateForm slug={slug}></PostCreateForm>
